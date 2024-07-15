de4b36e136d6f6fbd183f7d33dd0baa770ada359
nhamhongphuc <nhamphuc414@gmail.com>
2024-07-15 12:14:29 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,13 +15,13 @@ import { configValidationSchema } from './config.schema';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const isProduction = configService.get('STAGE') === 'prod';
+        // const isProduction = configService.get('STAGE') === 'prod';
 
         return {
-          ssl: isProduction,
-          extra: {
-            ssl: isProduction ? { rejectUnauthorized: false } : null,
-          },
+          ssl: { rejectUnauthorized: false },
+          // extra: {
+          //   ssl: { rejectUnauthorized: false },
+          // },
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
